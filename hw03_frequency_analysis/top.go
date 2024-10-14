29233fe4210ec03543e0aec31dbc3f63fa4b0c2d
ilya <malkow.ilya@gmail.com>
2024-10-14 18:44:18 +0300

    HW3 is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,51 @@
-package hw03frequencyanalysis
+package main
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"regexp"
+	"sort"
+	"strings"
+)
+
+type Words struct {
+	word  string
+	count int
+}
+
+var reg = regexp.MustCompile(`(-{2,})|([а-яА-Я]+-?[a-яА-Я]?)`)
+
+func Top10(text string) []string {
+	var result []string
+	wordList := strings.Fields(text)
+	words := make([]Words, 0, len(wordList))
+	wordMap := make(map[string]int, len(wordList))
+
+	for _, word := range wordList {
+		if word := reg.FindString(word); word != "" {
+			wordMap[strings.ToLower(word)]++
+		}
+	}
+
+	for word, count := range wordMap {
+		words = append(words, Words{word: word, count: count})
+	}
+
+	sort.Slice(words, func(i, j int) bool {
+		if words[i].count > words[j].count {
+			return true
+		}
+		if words[i].count == words[j].count {
+			return words[i].word < words[j].word
+		}
+		return false
+	})
+
+	if len(words) >= 10 {
+		words = words[:10]
+
+		for _, word := range words {
+			result = append(result, word.word)
+		}
+	}
+
+	return result
 }
